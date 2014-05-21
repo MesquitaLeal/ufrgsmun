f923cf02e3f9d8b046f9d774ac1c365f8a797cc0
Marcelo Leal <mesquitaleal@gmail.com>
2014-05-21 16:09:49 -0300

    Links Icon-Column

--- home.php
+++ home.php
@@ -28,18 +28,18 @@
 	<div class="row">
 		<div class="col-md-4">
 				<i class="fa fa-users fa-3x"></i>
-				<h6><a href="#">Meet us</a></h6>
+				<h6><a href="?page=about&section=meet">Meet us</a></h6>
 				<p>Meet the Secretariat of UFRGSMUN 2014</p>
 
 		</div>
 		<div class="col-md-4">
 			<i class="fa fa-desktop fa-3x"></i>
-			<h6><a href="#">Apply</a></h6>
-			<p>Registering for UFRGSMUN 2014 is really easy.<p>
+			<h6><a href="?page=apply">Apply</a></h6>
+			<p>Registering for UFRGSMUN 2014 is really easy.</p>
 		</div>
 		<div class="col-md-4">
 			<i class="fa fa-thumbs-up fa-3x"></i>
-			<h6><a href="#">Social Projects</a></h6>
+			<h6><a href="?page=about&section=social">Social Projects</a></h6>
 			<p>UFRGSMUN is engaged in several social activities.</p>
 		</div>
 </div>
