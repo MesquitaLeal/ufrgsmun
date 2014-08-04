67b0616007f285dbb0472e379fa02f33818ceb10
MesquitaLeal <mesquitaleal@gmail.com>
2014-08-04 09:18:45 -0300

    update country matrix

--- apply/countries.php
+++ apply/countries.php
@@ -16,17 +16,17 @@
 
     <div class="row blue content">
         <div class="col-sm-12 table-responsive">
-            <table class="table table-striped table-bordered">
+            <table class="table">
                 <tr>
-                    <th>Country</th>
+                    <th>Country*</th>
                     <th>ADB</th>
                     <th>UNSC</th>
                     <th>WSIS</th>
                     <th>DISEC</th>
                     <th>WEC</th>
                     <th>ZOPACAS</th>
-                    <th>CIJ</th>
-                    <th>IPC</th>
+                    <th>CIJ**</th>
+                    <th>IPC**</th>
                     <th>Total</th>
                 </tr>
 
@@ -43,6 +43,10 @@
 
                 } ?>
             </table>
+            <p class="references">* If there are more registrations than the vacancies on this list, the Secretariat holds the 
+                right to include countries which were not listed as stated on our General Policies, so as 
+                to guarantee the participation of all duly registered delegates. &nbsp | ** Be aware that although ICJ and IPC members are on the list, they do not represent any country.
+            <br/></p>
         </div>
     </div>
 
