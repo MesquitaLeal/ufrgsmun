5e4f0e4c68850b7fdd8e57220a4350b0f5d3dd81
Marcelo Leal <mesquitaleal@gmail.com>
2014-08-14 22:06:04 -0300

    updated sponsors

--- about/partners.php
+++ about/partners.php
@@ -3,20 +3,82 @@
         <h6>Home <i class="fa fa-angle-double-right"></i> About <i class="fa fa-angle-double-right"></i> Sponsors and Partners</h6>
     </div>
     <div class="row blue content">
-        <div class="section-header">
-        <h5>Sponsors</h5>
+        <div class="col-lg-12">
+            <section class="subsection-text">
+                <h5>Sponsors</h5>
+                <br/>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6">
+                        <a href="http://www.zeze.com.br/site/content/home/" class="thumbnail">
+                            <img src="images/sponsors/zeze.png" alt="Logo da Zezé">
+                        </a>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                        <a href="http://www.cafedomercado.com.br/" class="thumbnail">
+                            <img src="images/sponsors/cafe.png" alt="Logo do Café do Mercado">
+                        </a>
+                    </div>
+                </div>
+            </section>
+        </div>
     </div>
-        <div class="col-xs-12 col-sm-6">
-            <a href="#" class="thumbnail">
-                <img src="images/sponsors/zeze.png" alt="">
-            </a>
-        </div>
-        <div class="col-xs-12 col-sm-6">
-            <a href="#" class="thumbnail">
-                <img src="images/sponsors/cafe.png" alt="">
-            </a>
-        </div>
 
+    <svg id="bigTriangleColor" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 102" preserveAspectRatio="none">
+        <path d="M0 0 L50 100 L100 0 Z"></path>
+    </svg>
+
+    <div class="row navy-blue content">
+        <div class="col-lg-12">
+            <section class="subsection-text">
+                <h5>Support</h5>
+                <br/>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4">
+                        <a href="https://www.facebook.com/ceri.ufrgs" class="thumbnail">
+                            <img src="images/sponsors/ceri.jpg" alt="Logo do CERI" class="img-responsive">
+                        </a>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <a href="http://www.isape.org.br/index.php/pagina/home" class="thumbnail">
+                            <img src="images/sponsors/isape.png" alt="Logo do ISAPE">
+                        </a>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <a href="http://www.ufrgs.br/prorext/" class="thumbnail">
+                            <img src="images/sponsors/prorext.png" alt="Logo da PROREXT">
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4">
+                        <a href="http://www.ufrgs.br/fce/" class="thumbnail">
+                            <img src="images/sponsors/fce.png" alt="Logo da FCE" class="img-responsive">
+                        </a>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <a href="http://temasmg.com/" class="thumbnail">
+                            <img src="images/sponsors/temas.png" alt="Logo do Temas 10">
+                        </a>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <a href="http://amun.org.br/" class="thumbnail">
+                            <img src="images/sponsors/amun.png" alt="Logo do AMUN">
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4">
+                        <a href="http://famun.com.br/" class="thumbnail">
+                            <img src="images/sponsors/famun.jpg" alt="Logo do FAMUN" class="img-responsive">
+                        </a>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                        <a href="http://www.soi.org.br/" class="thumbnail">
+                            <img src="images/sponsors/soi.png" alt="Logo da SOI">
+                        </a>
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
 </div>
