cca29e653470b6c729b9f3d72a718f9cec75b934
Marcelo Leal <mesquitaleal@gmail.com>
2014-10-23 21:05:26 -0200

    Criação das páginas dos Study Guides

---
+++ preparation/study.php
@@ -0,0 +1,191 @@
+<div class="committees">
+    <div class="subpage-title">
+        <h6>Home <i class="fa fa-angle-double-right"></i> Preparation <i class="fa fa-angle-double-right"></i> Study Guides</h6>
+    </div>
+    <div class="row blue content">
+        <div class="col-lg-12">
+            <section class="subsection-text">
+                <h5>Study Guides</h5>
+                <p>
+                    In order to stimulate high level debates, UFRGSMUN staff publishes a set of study
+                    guides, each one covering one topic or topics to be discussed during the simulations.
+                    These guides are academic documents which should give delegates a basic preparation
+                    for the simulation. Delegates are extremely encouraged, though, to further research
+                    their committees’ topic before the event starts.
+                </p>
+                <br/>
+                <p>Click on the committees below to acess the study guides.</p>
+            </section>
+        </div>
+    </div>
+    <div class="row blue content">
+        <div class="panel-group" id="accordion">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                            General
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseOne" class="panel-collapse collapse in">
+                    <div class="panel-body">
+                        <div class="col-lg-12">
+                            <a href="?page=preparation&section=studyGuide">Complete Study Guide <i class="fa fa-arrow-circle-o-right fa-lg"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                            Board of Directors of the Asian Development Bank (ADB)
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseTwo" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <div class="col-lg-12">
+                            <a href="?page=preparation&section=adb">Maritime Connectivity in the Asia-Pacific Region <i class="fa fa-arrow-circle-o-right fa-lg"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+                            Disarmament and International Security Committee (DISEC)
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseThree" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <div class="col-sm-6">
+                            <a href="?page=preparation&section=disec_A">TOPIC A  <i class="fa fa-arrow-circle-o-right fa-lg"></i><br/>Foreign Military Bases</a>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="?page=preparation&section=disec_B">TOPIC B <i class="fa fa-arrow-circle-o-right fa-lg"></i><br/>Airspace Stabilization and Disarmament: the issue of military RPAS in foreign airspace</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+                            International Court of Justice (ICJ)
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse4" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <div class="col-sm-6">
+                            <a href="?page=preparation&section=icj_A">TOPIC A <i class="fa fa-arrow-circle-o-right fa-lg"></i><br/>Application of the Convention on the Prevention and Punishment of the Crime of Genocide (Croatia v. Serbia)</a>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="?page=preparation&section=icj_B">TOPIC B <i class="fa fa-arrow-circle-o-right fa-lg"></i><br/>Obligation to Negotiate Access to the Pacific Ocean (Bolivia v. Chile)</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">
+                            International Press Committee (IPC)
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse5" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <div class="col-sm-12">
+                            <a href="?page=preparation&section=ipc">Guidelines for IPC Journalists <i class="fa fa-arrow-circle-o-right fa-lg"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse6">
+                            United Nations Security Council (UNSC)
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse6" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <div class="col-sm-6">
+                            <a href="?page=preparation&section=unsc_A">TOPIC A  <i class="fa fa-arrow-circle-o-right fa-lg"></i><br/>The Situation Regarding Non-State Military Actors in the Middle East</a>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="?page=preparation&section=unsc_B">TOPIC B <i class="fa fa-arrow-circle-o-right fa-lg"></i><br/>The Situation in Iraq</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse7">
+                            World Energy Council (WEC)
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse7" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <div class="col-sm-6">
+                            <a href="?page=preparation&section=unsc_A">TOPIC A  <i class="fa fa-arrow-circle-o-right fa-lg"></i><br/>Shale Gas: Revolution?</a>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="?page=preparation&section=unsc_B">TOPIC B <i class="fa fa-arrow-circle-o-right fa-lg"></i><br/>Energy Transition and Challenges for the XXI Century</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse8">
+                            Ministerial Round Table of the World Summit on the Information Society Forum 2014 (WSIS)
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse8" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <div class="col-sm-6">
+                            <a href="?page=preparation&section=wsis_A">TOPIC A  <i class="fa fa-arrow-circle-o-right fa-lg"></i><br/>Internet Governance Surveillance</a>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="?page=preparation&section=wsis_B">TOPIC B <i class="fa fa-arrow-circle-o-right fa-lg"></i><br/>Legal and Political Measures to Address Cybercrime</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" data-parent="#accordion" href="#collapse9">
+                            Zona de Paz e Cooperação do Atlântico Sul (ZOPACAS)
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapse9" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <div class="col-sm-6">
+                            <a href="?page=preparation&section=zopacas_A">TOPIC A  <i class="fa fa-arrow-circle-o-right fa-lg"></i><br/>A Presená de Potência Extrarregionais no Atlântico Sul como ameaça à manutenção da ZOPACAS</a>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="?page=preparation&section=zopacas_B">TOPIC B <i class="fa fa-arrow-circle-o-right fa-lg"></i><br/>Pirataria no Golfo da Guiné</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+
+</div>
