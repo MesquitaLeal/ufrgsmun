0c5107d3c18155860a6e9b80ba4fe26d7e13fc68
Marcelo Leal <mesquitaleal@gmail.com>
2014-10-23 21:05:45 -0200

    Criação da página Handbook

--- preparation/handbook.php
+++ preparation/handbook.php
@@ -1,14 +1,14 @@
 <div class="committees">
     <div class="subpage-title">
-        <h6>Home <i class="fa fa-angle-double-right"></i> Preparation <i class="fa fa-angle-double-right"></i> Easy Handbook</h6>
+        <h6>Home <i class="fa fa-angle-double-right"></i> Preparation <i class="fa fa-angle-double-right"></i> Handbook</h6>
     </div>
     <div class="row blue content">
         <div class="col-lg-12">
             <section class="subsection-text">
-                <h5>Easy Handbook</h5>
-                <p style="text-align:center"><a href="../2014/files/easyHandbook.pdf">Click here</a> to download the Easy Handbook</p>
+                <h5>Handbook</h5>
+                <p style="text-align:center"><a href="../2014/files/handbook.pdf">Click here</a> to download the Handbook</p>
                 <br/>
-                <iframe src='https://onedrive.live.com/embed?cid=44A0DD5D2EA3F223&resid=44A0DD5D2EA3F223%21235&authkey=ADqi3Rh_3KAebMQ&em=2&wdStartOn=1&wdEmbedCode=0' width='476px' height='550px' frameborder='0'>Este é um PDF do <a target='_blank' href='http://office.com'>Microsoft Office</a> incorporado, da plataforma <a target='_blank' href='http://office.com/webapps'>Office Online</a>.</iframe>
+                <iframe src='https://onedrive.live.com/embed?cid=44A0DD5D2EA3F223&resid=44A0DD5D2EA3F223%21284&authkey=AAROUxpczfsyHKM&em=2&wdStartOn=1' width='476px' height='1100px' frameborder='0'>Este é um PDF do <a target='_blank' href='http://office.com'>Microsoft Office</a> incorporado, da plataforma <a target='_blank' href='http://office.com/webapps'>Office Online</a>.</iframe>
             </section>
         </div>
     </div>
--- preparation/index.php
+++ preparation/index.php
@@ -21,6 +21,10 @@ switch ($section) {
         include_once 'preparation/handbook.php';
         break;
 
+    case 'easyHandbook':
+        include_once 'preparation/easyHandbook.php';
+        break;
+
     case 'paper':
         include_once 'preparation/paper.php';
         break;
@@ -35,6 +39,70 @@ switch ($section) {
 
     case 'links':
         include_once 'preparation/links.php';
+        break;
+
+    case 'study':
+        include_once 'preparation/study.php';
+        break;
+
+    case 'studyGuide':
+        include_once 'preparation/studyGuide.php';
+        break;
+
+    case 'adb':
+        include_once 'preparation/adb.php';
+        break;
+
+    case 'disec_A':
+        include_once 'preparation/disec_A.php';
+        break;
+
+    case 'disec_B':
+        include_once 'preparation/disec_B.php';
+        break;
+
+    case 'icj_A':
+        include_once 'preparation/icj_A.php';
+        break;
+
+    case 'icj_B':
+        include_once 'preparation/icj_B.php';
+        break;
+
+    case 'ipc':
+        include_once 'preparation/ipc.php';
+        break;
+
+    case 'unsc_A':
+        include_once 'preparation/unsc_A.php';
+        break;
+
+    case 'unsc_B':
+        include_once 'preparation/unsc_B.php';
+        break;
+
+    case 'wec_A':
+        include_once 'preparation/wec_A.php';
+        break;
+
+    case 'wec_B':
+        include_once 'preparation/wec_B.php';
+        break;
+
+    case 'wsis_A':
+        include_once 'preparation/wsis_A.php';
+        break;
+
+    case 'wsis_B':
+        include_once 'preparation/wsis_B.php';
+        break;
+
+    case 'zopacas_A':
+        include_once 'preparation/zopacas_A.php';
+        break;
+
+    case 'zopacas_B':
+        include_once 'preparation/zopacas_B.php';
         break;
 
     }
